6d8e8a70304556a85b42329ac36b5cfe79d292cf
san4ows55 <san4ows55@gmail.com>
2019-12-14 13:58:10 +0300

    ajax methods w/o thymeleaf

---
+++ src/main/resources/static/js/filmsPage.js
@@ -0,0 +1,32 @@
+function test () {
+
+
+}
+
+$(document).ready(function () {
+    $.ajax({
+        url: '/api/film/all',
+        type: 'get',
+        contentType: 'application/json; charset=utf-8',
+        dataType: 'json',
+        success: function (films) {
+            console.log(films);
+            for (let i = 0; i < films.length; i++) {
+                $("#forAdd").before("<tr>" +
+                    "<td>" + films[i].id + "</td>" +
+                    "<td>" + films[i].title + "</td>" +
+                    "<td>" + films[i].rating + "</td>" +
+                    "<td>" + films[i].age + "</td>" +
+                    "<td>" +
+                    "<a class=\"btn btn-info\" href=\"/films/editFilm?id=" + films[i].id+"\">Edit</a>" +
+                    "<a class=\"btn btn-danger\" href=\"/films/deleteFilm?id=" + films[i].id+"\">Delete</a>" +
+                    "</td>" +
+                    "</tr>")
+            }
+
+        },
+        error: function (response) {
+            alert('error');
+        }
+    });
+});
